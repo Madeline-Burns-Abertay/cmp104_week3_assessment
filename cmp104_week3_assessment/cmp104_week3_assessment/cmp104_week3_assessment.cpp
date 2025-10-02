2a5d7acb49f3d888e00cc71a11472eea60e325f8
MICHAEL BURNS <2501892@abertay.ac.uk>
2025-10-02 17:04:15 +0100

    task complete
    all future commits will be report updates

--- cmp104_week3_assessment/cmp104_week3_assessment/cmp104_week3_assessment.cpp
+++ cmp104_week3_assessment/cmp104_week3_assessment/cmp104_week3_assessment.cpp
@@ -2,21 +2,28 @@
 using namespace std;
 
 int main() {
-	int height; char out;
+	int height; char outputChar;
 	cout << "enter single digit: "; cin >> height;
 	while (height < 1 || height > 9) {
-		cerr << "very funny";
+		cerr << "error: input is not a single digit" << endl;
 		cout << "enter single digit: "; cin >> height;
 	}
+	cout << endl;
 	for (uint8_t i = 1; i <= height; i++) {
 		for (uint8_t j = 1; j < 2 * height; j++) {
 			if (j <= height - i || j >= height + i) {
-				out = ' ';
+				outputChar = ' ';
 			}
 			else {
-				out = j - i + 0x30;
+				if (j <= height) {
+					outputChar = i - height + j;
+				}
+				else {
+					outputChar = height + i - j;
+				}
+				outputChar += 0x30;
 			}
-			cout << out;
+			cout << outputChar;
 		}
 		cout << endl;
 	}
