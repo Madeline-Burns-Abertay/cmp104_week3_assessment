c6d10e141b5fa604b023e25150058e731321f24c
MICHAEL BURNS <2501892@abertay.ac.uk>
2025-10-02 15:40:36 +0100

    update to version shown in problem1.png

---
+++ cmp104_week3_assessment/cmp104_week3_assessment/cmp104_week3_assessment.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int height; char out;
+	cout << "enter single digit: "; cin >> height;
+	while (height < 1 || height > 9) {
+		cerr << "very funny";
+		cout << "enter single digit: "; cin >> height;
+	}
+	for (uint8_t i = 1; i <= height; i++) {
+		for (uint8_t j = 1; j < 2 * height; j++) {
+			if (j <= height - i || j >= height + i) {
+				out = ' ';
+			}
+			else {
+				out = j - i + 0x30;
+			}
+			cout << out;
+		}
+		cout << endl;
+	}
+}
